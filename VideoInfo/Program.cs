2276de0232db1b900bc1f3e416aed64c3cf68ce5
Terry MacDonald <terry.macdonald@cosive.com>
2025-04-05 20:18:52 +1300

    Updated VideoInfo version to v1.8.9

--- VideoInfo/Program.cs
+++ VideoInfo/Program.cs
@@ -64,11 +64,11 @@ namespace VideoInfo
             NLog.LogManager.Configuration = config;
 
             // Start the Log file
-            SharedLogger.logger.Info($"VideoInfo/Main: Starting VideoInfo v1.8.8");
+            SharedLogger.logger.Info($"VideoInfo/Main: Starting VideoInfo v1.8.9");
             // Log the commandline options
             SharedLogger.logger.Info($"VideoInfo/Main: cmdline options: {string.Join(" ", args)}");
 
-            Console.WriteLine($"\nVideoInfo v1.8.8");
+            Console.WriteLine($"\nVideoInfo v1.8.9");
             Console.WriteLine($"=================");
             Console.WriteLine($"(c) Terry MacDonald 2025\n");
 
